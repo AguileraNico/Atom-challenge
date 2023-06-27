6e01fa22811e9cfbe18c67be354d413c8fd5104a
AguileraNico <aguileranicolas1487@gmail.com>
2023-06-27 12:02:57 -0300

    commit to see if stackblitz anda

--- src/sections/landing/landing.component.ts
+++ src/sections/landing/landing.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { AddNewModalComponent } from '@core/components/add-new/add-new.component';
+// import { AddNewModalComponent } from '@core/components/add-new/add-new.component';
 import { ToDoItem } from '@core/models/todo-item.model';
 import { ToDoItemService } from '@core/services/todo.service';
 
@@ -13,11 +13,18 @@ import { ToDoItemService } from '@core/services/todo.service';
   imports: [MatButtonModule, MatDialogModule],
 })
 export class LandingComponent {
-  constructor(private dialog: MatDialog, private todoService: ToDoItemService) {}
+  constructor(
+    private dialog: MatDialog,
+    private todoService: ToDoItemService
+  ) {}
 
   addNew = () => {
-    this.dialog.open(AddNewModalComponent).afterClosed().pipe(take(1)).subscribe((data: Omit<ToDoItem, 'id'>) => {
-      this.todoService.addItem(data)
-    });
+    this.dialog
+      .open(AddNewModalComponent)
+      .afterClosed()
+      .pipe(take(1))
+      .subscribe((data: Omit<ToDoItem, 'id'>) => {
+        // this.todoService.addItem(data)
+      });
   };
 }
